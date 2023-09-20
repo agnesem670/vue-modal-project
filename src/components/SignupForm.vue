7b72f575a4d70cc89d7e3a8da5102ffd4f228078
agnesem670 <agnesem670@gmail.com>
2023-09-20 10:20:05 +0200

    click event, deleting items from list

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -15,7 +15,7 @@
         <label>Skills:</label>
         <input type="text"  v-model="tempSkill" @keyup.alt="addSkill">
         <div v-for="skill in skills" :key="skill" class="pill">
-            {{ skill }}
+            <span @click="deleteSkill(skill)">{{ skill }}</span>
         </div>
     </form>
     <p>Email: {{ email }}</p>
@@ -47,6 +47,11 @@ export default {
                 this.tempSkill = ''
             }
         },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            })
+        }
     }
 }
 </script>
@@ -88,4 +93,17 @@ input[type="checkbox"] {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
-}</style>
+}
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+}
+</style>
